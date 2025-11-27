496fdb1fe6cb01ae96119325ffa505be475ea26a
Nanda Bagus Ramadhani <nannraamdh@gmail.com>
2025-11-27 19:13:53 +0700

    User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            // User Seeder
+            UserSeeder::class,
+
             //  Tentang Kami Seeders
             ProfilLembagaSeeder::class,
             VisiMisiSeeder::class,
